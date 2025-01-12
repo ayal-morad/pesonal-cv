fa3524978e9ff406fc39ea606344137da6388256
ayal-morad <ayalmrd@gmail.com>
2025-01-12 17:01:38 +0530

    fix error page

--- src/module/services/components/error.tsx
+++ src/module/services/components/error.tsx
@@ -1,3 +1,14 @@
+import { Typography } from "@mui/material";
+
 export function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="flex flex-1 flex-col justify-center items-center">
+      <Typography variant="h4" color="primary">
+        Error
+      </Typography>
+      <Typography variant="caption" color="secondary">
+        failed to reseve this page
+      </Typography>
+    </div>
+  );
 }
